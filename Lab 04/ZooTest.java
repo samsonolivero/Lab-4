1b1ebc12d44a92274b087801c5d3898462836d12
samsonolivero <47160208+samsonolivero@users.noreply.github.com>
2019-02-11 18:48:33 -0600

    Fifth Commit: Added and Made the ZooTest Class

--- Lab 04/ZooTest.java
+++ Lab 04/ZooTest.java
@@ -1,7 +1,35 @@
 public class ZooTest
 {
+	public void emptyConstructorTest() throws AssertException
+	{
+		Zoo zoo = new Zoo(0);
+		
+		Assert.assertEquals(0, zoo.getCapacity());
+		Assert.assertEquals(0.0, zoo.getTotalHeight());
+		Assert.assertEquals(0, zoo.getAverageWeight());
+		Assert.assertEquals(0, zoo.getAverageWeight("Blue"));
+		Assert.assertEquals(null, zoo.getAnimals());
+	}
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
+		Zoo zoo = new Zoo(2);
+		Animal ani = new Animal ("Orange", "Tiger", 16.0, 22.2);
+		Animal ani2 = new Animal ("Orange", "Bird", 5.2, 2.2);
+		Animal ani3 = new Animal ("Blue", "Penguin", 12.0, 3.0);
+		zoo.addAnimal(ani);
+		zoo.addAnimal(ani);
+		zoo.addAnimal(ani2);
+		zoo.addAnimal(ani3);
+		
+		System.out.println(zoo.toString());
+		
+		System.out.println("Capacity: " + zoo.getCapacity());
+		System.out.println("Total Height: " + zoo.getTotalHeight());
+		System.out.println("Average Weight: " + zoo.getAverageWeight());
+		System.out.println("Average Height: " + zoo.getAverageWeight("Orange"));
+		System.out.println("Animals: " + zoo.getAnimals());
+		
+		
+	
 	}
 }
